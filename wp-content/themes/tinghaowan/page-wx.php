df5fd847b49d8b65aa206f4071f7c04bf4f271d4
Uice Lu <uicestone@gmail.com>
2015-07-22 11:21:37 +0800

    消息回复文案修改

--- wp-content/themes/tinghaowan/page-wx.php
+++ wp-content/themes/tinghaowan/page-wx.php
@@ -7,6 +7,6 @@ $wx->verify();
 
 $wx->onmessage('event', function($message) use($wx){
 	if($message['EVENTKEY'] === 'COMING_SOON'){
-		$wx->reply_message('艇好玩微信公众号8月8日盛大发布，同时揭晓免费玩好艇的幸运玩家。', $message);
+		$wx->reply_message('艇好玩微信公众号8月底盛大发布，同时揭晓免费玩好艇的幸运玩家。', $message);
 	}
 });
